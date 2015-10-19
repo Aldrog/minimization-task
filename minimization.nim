bdf111a9f9a2dae6ea23992bc0687d72dd4893df
Andrew Penkrat <contact.aldrog@gmail.com>
2015-10-19 14:26:31 +0300

    Import function as f and use its members only in f.* form.

--- minimization.nim
+++ minimization.nim
@@ -1,18 +1,18 @@
-import function
+import function as f
 import math, linalg
 
-proc euclidNorm(x: Vector64[function.size]): float =
-    for i in 0..(function.size - 1):
+proc euclidNorm(x: Vector64[f.size]): float =
+    for i in 0..(f.size - 1):
         result = result + pow(x[i], 2.0)
     result = sqrt(result)
 
-proc estimateDistance(x: Vector64[function.size]): float = 1.0/delta * euclidNorm(A*x + b)
+proc estimateDistance(x: Vector64[f.size]): float = euclidNorm(f.A * x + f.b) / f.delta
 
-proc gradDescent*(precision: float): tuple[value: float, point: Vector64[function.size]] =
-    result.point = zeros(size)
+proc gradDescent*(precision: float): tuple[value: float, point: Vector64[f.size]] =
+    result.point = zeros(f.size)
     while estimateDistance(result.point) > precision:
-        let q = A*result.point + b
-        let mu = -(pow(euclidNorm(A*result.point + b), 2.0) / ((A.t*(A*result.point + b)).asMatrix(1,size)*(A*result.point + b))[0])
+        let q = f.A * result.point + f.b
+        let mu = -(pow(euclidNorm(q), 2.0) / ((f.A.t * q).asMatrix(1, f.size) * q)[0])
         result.point = mu*q + result.point
-        echo(result.point, "\n", function.f(result.point))
-    result.value = function.f(result.point)
+        echo(result.point, "\n", f(result.point))
+    result.value = f(result.point)

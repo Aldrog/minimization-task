62369c0f0b992a8812d26de4fd6cd05eef391440
Andrew Penkrat <contact.aldrog@gmail.com>
2015-10-19 16:05:07 +0300

    Move gradient calculation to procedure in function module.

--- minimization.nim
+++ minimization.nim
@@ -6,7 +6,7 @@ proc euclidNorm(x: Vector64[f.size]): float =
         result = result + pow(x[i], 2.0)
     result = sqrt(result)
 
-proc estimateDistance(x: Vector64[f.size]): float = euclidNorm(f.A * x + f.b) / f.delta
+proc estimateDistance(x: Vector64[f.size]): float = euclidNorm(f.grad(x)) / f.delta
 
 proc gradDescent*(precision: float): tuple[value: float, point: Vector64[f.size]] =
     echo("Gradient descent method")
@@ -14,7 +14,7 @@ proc gradDescent*(precision: float): tuple[value: float, point: Vector64[f.size]
     echo("Starting point:\n", result.point, "\nf = ", f(result.point))
     var step = 1
     while estimateDistance(result.point) > precision:
-        let q = f.A * result.point + f.b
+        let q = f.grad(result.point)
         let mu = -(pow(euclidNorm(q), 2.0) / ((f.A.t * q).asMatrix(1, f.size) * q)[0])
         result.point = mu*q + result.point
         echo("\nStep #", step, ":\n", result.point, "\nf = ", f(result.point))

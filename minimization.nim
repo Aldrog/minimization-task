959e9e3a169720942c1bb95cfe9705667665574f
Andrew Penkrat <contact.aldrog@gmail.com>
2015-10-27 15:01:56 +0300

    Replace custom norm with built-in one. Missed it while reading documentation.

--- minimization.nim
+++ minimization.nim
@@ -1,12 +1,7 @@
 import function as f
 import math, linalg
 
-proc euclidNorm(x: Vector64[f.size]): float =
-    for i in 0..(f.size - 1):
-        result = result + pow(x[i], 2.0)
-    result = sqrt(result)
-
-proc estimateDistance(x: Vector64[f.size]): float = euclidNorm(f.grad(x)) / f.delta
+proc estimateDistance(x: Vector64[f.size]): float = l_2(f.grad(x)) / f.delta
 
 proc gradDescent*(precision: float): tuple[value: float, point: Vector64[f.size]] =
     stderr.writeLine("\n@ Gradient descent method @\n")
@@ -15,7 +10,7 @@ proc gradDescent*(precision: float): tuple[value: float, point: Vector64[f.size]
     var step = 1
     while estimateDistance(result.point) > precision:
         let q = f.grad(result.point)
-        let mu = -(pow(euclidNorm(q), 2.0) / (q.asMatrix(1, f.size) * A * q)[0])
+        let mu = -(pow(l_2(q), 2.0) / (q.asMatrix(1, f.size) * A * q)[0])
         result.point = mu*q + result.point
         stderr.writeLine("Step #", step, ":\n", result.point, "\nf = ", f(result.point), "\n")
         step += 1
